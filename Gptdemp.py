f00eafd900623f842a14cd81eea6a7a4d1a86c43
Zakaria989 <Zakaria0909@hotmail.com>
2023-06-30 16:10:54 +0200

    Added extra features, like meal planner and workout planner

--- Gptdemp.py
+++ Gptdemp.py
@@ -6,7 +6,7 @@ import re
 
 
 
-openai.api_key = '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'
+openai.api_key = 'sk-'
 
 file_path_context = os.getcwd() + "/user_context.txt"
 file_path_context_history = os.getcwd() + "/context_history.txt"
@@ -18,6 +18,12 @@ user_age = 0
 user_weight = 0.0
 user_height = 0.0
 user_bmi = 0.0
+user_emoji = "🤠"
+
+meal_plan = ""
+workout_plan = ""
+
+
 
 
 with open(file_path_context,"r",  encoding="utf8") as file:
@@ -55,12 +61,19 @@ def calculate_age(birth_date):
 
 def meal_planner_prompt(diet_preference,snack_preference, meal_amount_preference, other_preference ):
     prompt = f'''
-Create a meal plan for seven days, where for each day there is only {meal_amount_preference}, the snack amount for each day is {snack_preference}.
+Create a meal plan for seven days, where for each day there is only {meal_amount_preference}, so for monday {meal_amount_preference} and so on. 
+Make the calorie amount fitting for each person, with each meal amount. We dont want them to lose or gain weight.
+the snack amount for each day is {snack_preference}.
 The person who we are making the plan for has a diet preference like {diet_preference}, other preferences are {other_preference} 
-please adhere to those preferences
+please adhere to those preferences.
+
+Make it clear how much protein, fat, and carbohydrate they need to reach their goal and then steering them towards an app like myfitnesspal (link takes you to site), 
+that makes it easy for them to easily log the details can make a huge difference to people’s lives.
     '''
     assistant_answer = respond_bot(prompt)
+    
     return assistant_answer
+
 
 def workout_planner_prompt(amount_of_days, workout_intensity,cardio_option, strength_training, workout_duration, other_factors):
     prompt = f'''
@@ -75,6 +88,52 @@ Design a workout plan that aligns with the given specifications and provides eff
     '''
     assistant_answer = respond_bot(prompt)
     return assistant_answer
+
+
+
+def recipie_prompt(recipe, portions):
+    
+    diffouclty_of_recipie_prompt = f''''
+What is the diffcuilty of this recipie {recipe}
+give me the rating in terms off:
+Level 1: The easiest of the levels. This level does not contain alcohol or require the use of a heat source (stove, grill, etc) making it safe for children to prepare with limited assistance. 
+    Contains only a limited number of ingredients and steps to complete the recipe.
+
+Level 2: Very easy but may require the use of a heat source and may contain alcohol. Easy to find ingredients and simple steps involved in making the recipe. 
+    Older children may be able to make (non-alcoholic!) recipes that fall into this category. Recipes can usually be made by people with little to no cooking experience.
+Level 3: Average. The majority of the recipes on CDKitchen fall into this category. Most recipes will require basic cooking skills (chopping, dicing, measuring, use of small appliances, etc)
+    and contain ingredients that most home cooks have access to in regular grocery stores. This by no means indicates these recipes don't fall under the category of "gourmet", it just usually means the work involved isn't as hard as it sounds.
+Level 4: Above-average. These recipes either contain harder to find ingredients (that require a specialty store), more advanced cooking skills, or unusual tools needed to prepare the recipe.
+    Many of the recipes can be made by an "average" cook but finding some of the ingredients may take some extra shopping. Some of the recipes contain basic ingredients but require extra cooking skills or special tools. 
+    Recipes that may contain significantly more preparation and assembly steps may also fall into this category.
+Level 5: Difficult. For those looking for a challenge! These recipes usually contain unusual ingredients and require advanced cooking skills or elaborate preparations. 
+    Not all recipes in this category are impossible but rather require more dedication from the cook!   
+    '''
+    amount_of_time_for_recipie_prompt = f'''
+How long would it take to create the recipe of {recipe}
+    '''
+    
+    recipie_prompt = f'''
+Give me a recipe for {recipe}, that will feed {portions} people, include the price of each ingredient
+    '''
+    recipe_pic = respond_picture(f"Realistic picture of this recipie {recipe} on a white table, with a white plate with the food on it")
+    
+    assistant_answer_recipie = respond_bot(recipie_prompt)
+    assistant_answer_difficulty = respond_bot(diffouclty_of_recipie_prompt)
+    assistant_answer_time = respond_bot(amount_of_time_for_recipie_prompt)
+    return assistant_answer_recipie, assistant_answer_difficulty, assistant_answer_time, recipe_pic
+
+
+
+def exercise_prompt(exercise, weights):
+    
+    prompt = f''''
+Write a step by step exercise guide for {exercise}, with {weights}  
+    '''
+    exercise_pic = respond_picture(f"Realistic picture of this exercise {exercise}")
+    
+    assistant_answer_exercise = respond_bot(prompt)
+    return assistant_answer_exercise, exercise_pic
 
 
 
@@ -96,11 +155,20 @@ def add_user_info_to_file():
     user_bmi = calculate_bmi(float(user_weight), float(user_height))
     # Replace the placeholders in the content with user information
     content = f'''You are an absolute all knowing personal health/personal/chef assistant  dedicated to helping individuals achieve their fitness goals and improve their overall well-being.
-You provide personalized guidance, tips, and recommendations based on their specific needs, only answer health, fitness and personal questions.
+You provide personalized guidance, tips, and recommendations based on their specific needs, only answer health, fitness and personal questions. Do not answer questions if they are not realted
+to the users health.
+As the personal trainer you offer:
+Exercise knowledge and development
+Frienship
+Support
+Motivation 
+Reassuernce 
+
+
 To answer questions use the information below.
 
 
-User Information:
+User Information below when making plans and talking back to the user:
     
 - Sex: {(user_sex)}
 - Age: {user_age}
@@ -160,10 +228,10 @@ def input_question(question):
 
 
 
-def respond_chatbot (message, chat_history):
+def respond_chatbot (message, chat_history, emoji, name):
         
     bot_message = input_question(message)
-    chat_history.append((message, "Coach: " + bot_message))
+    chat_history.append(( f"{str(emoji)}: "+ message, " 🏋️‍♂️: " + bot_message))
     time.sleep(2)
     return "", chat_history
 
@@ -172,6 +240,16 @@ def respond_bot(message):
     bot_message = input_question(message)
     time.sleep(2)
     return bot_message
+
+
+def respond_picture(question):
+    response = openai.Image.create(
+    prompt=question,
+    n=1,
+    size="256x256"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
 
 # Functions for checking user values
 def update_visibility():
@@ -246,6 +324,8 @@ def check_all(name_textbox, birth_date_textbox, height_textbox,weight_textbox):
     return gr.Column.update(visible=True), gr.Column.update(visible=False) 
 
 
+    
+
 message_history =[{"role": "system", "content": context},
             ]
 
@@ -304,7 +384,9 @@ class FactoryBot():
         yield self.session.chat_completion(question)
         
  
-
+def transcribe(audio):
+    trans = openai.Audio.transcribe("whisper-1",audio)
+    return trans
     
 
 with gr.Blocks(theme=gr.themes.Soft(),title="Health Assistant", css=".footer {display:none !important}") as demo:
@@ -314,11 +396,15 @@ with gr.Blocks(theme=gr.themes.Soft(),title="Health Assistant", css=".footer {di
     
     with column1:
         
-        sex = gr.Dropdown(["Male", "Female", "Undefined"], label="Gender", value="Male")
+        sex = gr.Dropdown(["Male", "Female", "Undefined"], label="Gender", value= "Male")
         birth_date = gr.Textbox(label="Birth Date", placeholder="dd.mm.yyyy", value="27.07.1998")
-        name = gr.Textbox(label="Name", placeholder="John Doe", value="Zakaria Ahmed")
-        weight = gr.Textbox(label="Weight", placeholder="Kg", value="80")
+        name = gr.Textbox(label="Name", placeholder="John Doe", value="Zakaria")
+        weight = gr.Textbox(label="Weight", placeholder="Kg", value="80" )
         height = gr.Textbox(label="Height", placeholder="cm", value="177")
+        
+        user_emoji = gr.Dropdown(["😃", "😄", "😊", "😎", "😍", "🙂", "🤩", "😇", "😊", "😉", "🤗", "😌", "😀", "😁", "😆", "😋", "😏"], 
+                                    label="Pick an Emoji to represent you in the chat", value = "😃")        
+        
         b = gr.Textbox(value="No Errors", label= "Error Box")
         
         btn_1 = gr.Button(label="Submit", variant="primary") 
@@ -342,7 +428,7 @@ with gr.Blocks(theme=gr.themes.Soft(),title="Health Assistant", css=".footer {di
             with gr.Tab("Chatbot"):
                 chatbot = gr.Chatbot(show_label=True).style(height=750)
                 msg = gr.Textbox()
-                msg.submit(respond_chatbot, [msg, chatbot], [msg,chatbot])
+                msg.submit(respond_chatbot, [msg, chatbot, user_emoji, name], [msg,chatbot])
                 
             with gr.Tab("Meal planner"):
                 diet_preference = gr.CheckboxGroup(["Vegan", "Pescitarian", "Keto", "Vegetarian", "Flexitarian", "Mediterranean", "Paleolithic (Paleo)", "Whole30", "Gluten-free", "Dairy-free", "Low-carb", "Low-fat", "DASH"],
@@ -382,7 +468,49 @@ with gr.Blocks(theme=gr.themes.Soft(),title="Health Assistant", css=".footer {di
                 btn_3.click(fn=workout_planner_prompt, inputs=[workout_days, workout_intensity, cardio_options, strength_training, workout_duration,other_factors_workout ],
                             outputs=workout_plan_output)
 
-        
+            with gr.Tab("Recipe"):
+                
+                recipe = gr.Textbox(label="What meal do you want to cook")
+                portion_amount = gr.Dropdown(["1","2","3", "4","5","6"],label="Portions")
+                
+                
+                btn_4 = gr.Button(label="Submit", variant="primary")
+                
+                difficulty_out = gr.Textbox(label="Difficulty:")
+                time_out = gr.Textbox(label="Time to create:")
+                recipe_out = gr.TextArea(label="The recipe:", scroll_to_output = True)
+                
+
+                recipe_picture = gr.Image(shape=[256,256])
+                btn_4.click(fn=recipie_prompt, inputs=[recipe, portion_amount], outputs=[recipe_out, difficulty_out, time_out, recipe_picture])
+                
+                
+            with gr.Tab("Exercise"):
+                
+                exercise = gr.Textbox(label="What exercise do you want to know more about")
+                weight_amount = gr.Dropdown(["Wights", "No wights"],label="Are you using weights")
+                
+                btn_4 = gr.Button(label="Submit", variant="primary")
+                
+                exercise_tips = gr.TextArea(label="The recipe:", scroll_to_output = True)
+                
+
+                exercise_picture = gr.Image(shape=[256,256])
+                btn_4.click(fn=exercise_prompt, inputs=[exercise, weight_amount], outputs=[exercise_tips, exercise_picture])
+                
+
+                
+            with gr.Tab("Voice assistant"):
+                
+                mic = gr.Audio(source="microphone")
+                btn_5 = gr.Button(label=("Submit"), variant="primary")
+                
+                
+                out_text = gr.TextArea(label="Return Text:",)
+                
+                btn_5.click(fn=transcribe, inputs= mic, outputs= out_text)
+                
+                
+
 if __name__ == "__main__":  
     demo.launch()
-   
